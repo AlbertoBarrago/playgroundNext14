070768a93e6c9f73a9367364bbf3bd0ec871ad0b
Alberto Barrago <alberto.barrago@softfobia.com>
2023-11-09 22:45:26 +0100

    added D&D

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,8 +1,10 @@
 import {Button} from "../@/components/ui/button"
 import {CardTitle, CardHeader, CardContent, CardFooter, Card} from "../@/components/ui/card"
-import {useCallback, useEffect, useState} from "react";
+import React, {useState} from "react";
 import CardComponents from "../components/card";
-import {log} from "util";
+
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import {DndProvider, useDrag} from 'react-dnd'
 
 export type status = "todo" | "doing" | "done";
 export type type = "TODO" | "DOING" | "DONE";
@@ -21,6 +23,31 @@ export interface CardInterface {
     visible: boolean;
     listElement: EventInterface[];
 }
+
+const CardItem: React.FC<{ card: CardInterface }> = ({ card }) => {
+    const [, drag] = useDrag({
+        type: 'CARD',
+        item: { card },
+    });
+
+    return (
+        <Card ref={drag}>
+            <CardHeader>
+                <CardTitle className="text-black">{card.title}</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-2">
+                {card.listElement.map((eCard) => (
+                    <CardComponents key={eCard.id} {...eCard} />
+                ))}
+            </CardContent>
+            <CardFooter>
+                <Button className="text-black" variant="secondary" size="sm">
+                    Add Card
+                </Button>
+            </CardFooter>
+        </Card>
+    );
+};
 
 export default function Component() {
     const [cardList, setCardList] = useState<CardInterface[]>([
@@ -44,7 +71,7 @@ export default function Component() {
             visible: true,
             listElement: [
                 {
-                    id: 2.1,
+                    id: 1.2,
                     type: "DOING",
                     title: "titolo prova doing",
                     description: "",
@@ -58,7 +85,7 @@ export default function Component() {
             visible: true,
             listElement: [
                 {
-                    id: 3.1,
+                    id: 1.3,
                     type: "DONE",
                     title: "titolo prova done",
                     description: "",
@@ -69,9 +96,8 @@ export default function Component() {
 
     ])
 
-
     return (
-        <section className="h-screen w-full bg-gray-100 dark:bg-gray-800">
+        <DndProvider debugMode={true} backend={HTML5Backend}>
             <header className="flex items-center justify-between px-6 py-4 bg-white shadow-md dark:bg-gray-900">
                 <div className="flex items-center space-x-2">
                     <svg
@@ -97,30 +123,13 @@ export default function Component() {
                     <Button className="text-black">Sign Up</Button>
                 </div>
             </header>
-            <main className="p-6 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                {cardList && cardList.map((card) => {
-                    return (
-                            <Card key={card.id}>
-                                <CardHeader>
-                                    <CardTitle className="text-black">{card.title}</CardTitle>
-                                </CardHeader>
-                                <CardContent className="space-y-2">
-                                    {card.listElement.map((eCard, eIndex) => {
-                                        return (
-                                            <CardComponents
-                                                key={eCard.id}
-                                                {...eCard}
-                                            />
-                                        )
-                                    })}
-                                </CardContent>
-                                <CardFooter>
-                                    <Button className="text-black" variant="secondary" size="sm">Add Card</Button>
-                                </CardFooter>
-                            </Card>
-                    )
-                })}
-            </main>
-        </section>
-    )
+            <section className="h-screen w-full bg-gray-100 dark:bg-gray-800">
+                <main className="p-6 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                    {cardList && cardList.map((card) => (
+                         <CardItem key={card.id} card={card} />
+                    ))}
+                </main>
+            </section>
+        </DndProvider>
+    );
 }
